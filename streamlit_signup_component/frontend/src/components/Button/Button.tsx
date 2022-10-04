a228e3652cb0704767bf3625dcfdb36e4664d8e1
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2022-10-04 12:50:13 +0530

    update types, prop naming and styling

--- streamlit_signup_component/frontend/src/components/Button/Button.tsx
+++ streamlit_signup_component/frontend/src/components/Button/Button.tsx
@@ -3,26 +3,26 @@ import React, { ReactNode } from "react"
 import "./Button.css"
 
 interface PropTypes {
-  type: string;
+  type?: string;
   children: ReactNode;
   handleClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
-  isDisabled: boolean;
+  isDisabled?: boolean;
 }
 
 const Button = ({
-    type,
+    type = "primary",
     children,
     handleClick,
-    isDisabled,
-    ...props
-}: PropTypes): ReactNode => {
+    isDisabled = false,
+    ...rest
+}: PropTypes) => {
   return (
     <div className="custom-button-container">
       <button
         className={`custom-button custom-button-${type}`}
         onClick={handleClick}
         disabled={isDisabled}
-        {...props}
+        {...rest}
       >
         {children}
       </button>
