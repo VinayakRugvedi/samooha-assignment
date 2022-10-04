0585dd3c9d509e7576930f707e3bf17665eb164c
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2022-10-04 12:52:51 +0530

    create components directory and add Form component

---
+++ streamlit_signup_component/frontend/src/Signup/components/Form/Form.container.tsx
@@ -0,0 +1,75 @@
+import React, {ReactNode, useState} from "react";
+
+import Form from "./Form"
+
+interface PropTypes {
+  tos_link: string;
+  privacy_link: string;
+}
+
+const FormNameTypes = {
+  AccountLocator: "accountLocator",
+  Username: "username",
+  Password: "password"
+}
+
+const FormContainer = ({
+  tos_link,
+  privacy_link
+}: PropTypes) => {
+  const [formData, setFormData] = useState({
+    [FormNameTypes.AccountLocator]: {
+      placeHolder: "Account Locator",
+      value: "",
+      errorString: ""
+    },
+    [FormNameTypes.Username]: {
+      placeHolder: "Username",
+      value: "",
+      errorString: ""
+    },
+    [FormNameTypes.Password]: {
+      placeHolder: "Password",
+      value: "",
+      errorString: ""
+    }
+  })
+
+  const handleUpdate = (name: string, value: string): void => {
+    const formDataCopy = {...formData}
+    formDataCopy[name].value = value
+    formDataCopy[name].errorString = ""
+
+    if (value.length === 0) {
+      formDataCopy[name].errorString = "This field cannot be empty."
+      setFormData(formDataCopy)
+      return
+    }
+
+    if (name === FormNameTypes.AccountLocator && value.length > 12) {
+      formDataCopy[name].errorString = "Account Locator cannot exceed 12 characters."
+    } else if (name === FormNameTypes.Username && value.length < 5) {
+      formDataCopy[name].errorString = "Username should be at least 5 characters."
+    } else if (name === FormNameTypes.Password && value.length < 8) {
+      formDataCopy[name].errorString = "Password should be at least 8 characters."
+    }
+
+    setFormData(formDataCopy)
+  }
+
+  const handleSubmit = (): void => {
+    // handleSubmit
+  }
+
+  return (
+    <Form
+      formData={formData}
+      handleUpdate={handleUpdate}
+      handleSubmit={handleSubmit}
+      tos_link={tos_link}
+      privacy_link={privacy_link}
+    />
+  )
+}
+
+export default FormContainer
---
+++ streamlit_signup_component/frontend/src/Signup/components/Form/Form.tsx
@@ -0,0 +1,69 @@
+import React, {ReactNode} from "react";
+
+import {
+  Button,
+  InputText
+} from "components"
+import "./Form.css"
+
+interface PropTypes {
+  formData: object;
+  handleUpdate: (name: string, value:string) => void;
+  handleSubmit: () => void;
+  tos_link: string;
+  privacy_link: string;
+}
+
+const Form = ({
+  formData,
+  handleUpdate,
+  handleSubmit,
+  tos_link,
+  privacy_link
+}: PropTypes) => {
+  let isButtonDisabled = false
+  const formInputsContent = []
+  
+  for (const [name, data] of Object.entries(formData)) {
+    if (!isButtonDisabled && data.value.length === 0) {
+      isButtonDisabled = true
+    }
+
+    formInputsContent.push(
+      <div className="input-wrapper">
+        <InputText
+          placeHolder={data.placeHolder}
+          value={data.value}
+          errorString={data.errorString}
+          handleChange={(value: string) => handleUpdate(name, value)}
+          style={{width: "25rem"}}
+        />
+      </div>
+    )
+  }
+
+  return (
+    <form className="signup-form-container">
+      <div className="inputs-container">
+        {formInputsContent}
+      </div>
+      <div className="terms-policy-container">
+        <p className="terms-policy-text">
+          By signing up, I agree to the&nbsp;
+          <strong><a href={tos_link} target="_blank">Terms of Service</a></strong> and&nbsp;
+          <strong><a href={privacy_link} target="_blank">Privacy Policy</a></strong>
+        </p>
+      </div>
+      <div className="button-container">
+        <Button
+          handleClick={handleSubmit}
+          isDisabled={isButtonDisabled}
+        >
+          Sign up
+        </Button>
+      </div>
+    </form>
+  )
+}
+
+export default Form
